146ac8edc1243623338e33cf4cc4b453ced80c72
egoriy69 <egoriy699@gmail.com>
2024-01-11 12:20:02 +0400

    йоу

--- diplom34/src/main/java/com/example/diplom33/services/EmployeeService.java
+++ diplom34/src/main/java/com/example/diplom33/services/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.example.diplom33.services;
 
 import com.example.diplom33.dto.EmpRegClientDTO;
+import com.example.diplom33.models.Client;
+import com.example.diplom33.models.Employee;
 import com.example.diplom33.models.User;
+import com.example.diplom33.repositories.ClientRepository;
+import com.example.diplom33.repositories.EmployeeRepository;
 import com.example.diplom33.repositories.RoleRepository;
 import com.example.diplom33.repositories.UserRepository;
 import lombok.AllArgsConstructor;
@@ -14,6 +18,9 @@ import java.util.Collections;
 public class EmployeeService {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final ClientRepository clientRepository;
+    private final EmployeeRepository employeeRepository;
+
     public void createUser(EmpRegClientDTO empRegClientDTO){
         User user = new User();
         user.setFirstname(empRegClientDTO.getFirstName());
@@ -24,9 +31,23 @@ public class EmployeeService {
         user.setPassport(empRegClientDTO.getPassport());
         user.setEmail(empRegClientDTO.getEmail());
         user.setRoles(Collections.singletonList(roleRepository.findByName(empRegClientDTO.getRole()).get()));
-        userRepository.save(user);
+        createUserByRole(user, empRegClientDTO);
+
     }
 
-
+    private void createUserByRole(User user, EmpRegClientDTO empRegClientDTO){
+        if(empRegClientDTO.getRole().equals("ROLE_CLIENT")){
+            Client client = new Client();
+            client.setUser(user);
+            userRepository.save(user);
+            clientRepository.save(client);
+        }
+        else {
+           Employee employee = new Employee();
+           employee.setUser(user);
+           userRepository.save(user);
+           employeeRepository.save(employee);
+        }
+    }
 
 }
