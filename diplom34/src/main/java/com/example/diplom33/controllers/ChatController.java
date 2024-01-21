21d00fadebcd8851fc1de6f1989f31367085dee2
egoriy69 <egoriy699@gmail.com>
2024-01-21 21:20:20 +0400

    qqq

---
+++ diplom34/src/main/java/com/example/diplom33/controllers/ChatController.java
@@ -0,0 +1,30 @@
+package com.example.diplom33.controllers;
+
+import com.example.diplom33.chat.ChatMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class ChatController {
+
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        System.out.println("tyt");
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
